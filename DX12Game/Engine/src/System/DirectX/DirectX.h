8cbeb5975ae90a45fed6df73759042ba118c200e
4A-14\koyo <a>
2026-05-29 19:29:18 +0900

    コメント追加

--- DX12Game/Engine/src/System/DirectX/DirectX.h
+++ DX12Game/Engine/src/System/DirectX/DirectX.h
@@ -11,6 +11,8 @@ void SafeRelease(T*& t)
 	}
 }
 
+using Microsoft::WRL::ComPtr;
+
 /// <summary>
 /// DirectX用クラス
 /// </summary>
@@ -70,7 +72,7 @@ public:
 	/// </summary>
 	ID3D12Device* GetDevice()
 	{
-		return Device;
+		return Device.Get();
 	}
 
 	/// <summary>
@@ -78,7 +80,7 @@ public:
 	/// </summary>
 	ID3D12GraphicsCommandList* GetCommandList()
 	{
-		return CommandList;
+		return CommandList.Get();
 	}
 
 private:
@@ -151,66 +153,67 @@ private:
 
 private:
 
+	// 生ポ回避
 
 	/// <summary>
 	/// Dx12デバイス
 	/// </summary>
-	ID3D12Device* Device;
+	ComPtr<ID3D12Device> Device;
 
 	/// <summary>
 	/// Dx12ファクトリー
 	/// </summary>
-	IDXGIFactory6* Factory;
+	ComPtr<IDXGIFactory6> Factory;
 
 	/// <summary>
 	/// Dx12スワップチェイン
 	/// </summary>
-	IDXGISwapChain4* SwapChain;
+	ComPtr<IDXGISwapChain4> SwapChain;
 
 	/// <summary>
 	/// Dx12コマンドアロケーター
 	/// </summary>
-	ID3D12CommandAllocator* CommandAllocator;
+	ComPtr<ID3D12CommandAllocator> CommandAllocator;
 
 	/// <summary>
 	/// Dx12コマンドリスト
 	/// </summary>
-	ID3D12GraphicsCommandList* CommandList;
+	ComPtr<ID3D12GraphicsCommandList> CommandList;
 
 	/// <summary>
 	/// Dx12コマンドキュー
 	/// </summary>
-	ID3D12CommandQueue* CommandQueue;
+	ComPtr<ID3D12CommandQueue> CommandQueue;
 
 	/// <summary>
 	/// Dx12バックバッファー
 	/// </summary>
-	ID3D12Resource* BackBuffers[NumBackBuffer];
+	ComPtr<ID3D12Resource> BackBuffers[NumBackBuffer];
 
 	/// <summary>
 	/// Dx12深度バッファ
 	/// </summary>
-	ID3D12Resource* DepthBuffer;
+	ComPtr<ID3D12Resource> DepthBuffer;
 
 	/// <summary>
 	/// Dx12バックバッファ用ヒープ
 	/// </summary>
-	ID3D12DescriptorHeap* BackBufferHeap;
+	ComPtr<ID3D12DescriptorHeap> BackBufferHeap;
 
 	/// <summary>
 	/// Dx12深度バッファ用ヒープ
 	/// </summary>
-	ID3D12DescriptorHeap* DepthHeap;
+	ComPtr<ID3D12DescriptorHeap> DepthHeap;
 
 	/// <summary>
 	/// Dx12フェンス
 	/// </summary>
-	ID3D12Fence* Fence;
+	ComPtr<ID3D12Fence> Fence;
 
 	/// <summary>
 	/// メモリ解放漏れ検知用
 	/// </summary>
-	ID3D12DebugDevice* DebugDevice;
+	ComPtr<ID3D12DebugDevice> DebugDevice;
 
 	/// <summary>
 	/// フェンス用
